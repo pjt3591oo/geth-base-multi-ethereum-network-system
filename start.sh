f56921b54468a0a35aa0f31482529f14fd47d500
pjt3591oo <pjt3591oo@gmail.com>
2018-04-01 21:46:02 +0900

    각 노드의 환경변수와 command를 수정한다

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 # docker build -t ethereum .
-# docker-compose up, cocker-compost up -d
+# docker-compose up, docker-compost up -d
 # 컨테이너 접속 docker exec -it container_name /bin/bash
 # 컨테이너 생성 docker run -it container_name /bin/bash
 
 echo "$1"
+# geth --networkid 4649 --maxpeers 3 --datadir /home/DATA_STORE console
